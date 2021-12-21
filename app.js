ca16621ae68056323ab526e9e5dea19e3e2e32c8
Cristian Montes <cmontesmujica@gmail.com>
2021-12-20 23:16:17 -0700

    went over core types in ts

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Your Code Goes Here....');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 45;
+var number2 = 2.34;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
+console.log(result);
