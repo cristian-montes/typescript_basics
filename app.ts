4874631cf82aa8608000260da927a3662560f4a5
Cristian Montes <cmontesmujica@gmail.com>
2021-12-20 23:26:18 -0700

    object data types

--- app.ts
+++ app.ts
@@ -1,18 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string){
-    const result = n1 + n2;
-    if(showResult) {
-        console.log(phrase +  result);
-    } else {
-        return result;
+// this is best practice when declaring types in a object. You don't want to go and specifically call every single data type in an objec because of redudantcy. it is still valid tho. 
+const product = {
+    id: '123n',
+    price: 12.99,
+    tags:['great offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
     }
 }
-
-const number1 = 45; // Type inferance -> the reason why we dont need a specify the data type of these const since their value would not change. In other words it will be because you initiallize with a number in this example
-// Only declare the data type if the const it is not given an initial value, then you can do this let square: number; so we know it has to take a number type as value;
-
-const number2 = 2.34;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-const result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
